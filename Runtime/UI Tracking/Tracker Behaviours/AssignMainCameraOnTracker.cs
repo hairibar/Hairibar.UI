1871eccad9e6690fbe4cb1959b91178a17685cef
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-03 14:41:02 +0100

    Added AssignMainCameraOnTracker

---
+++ Runtime/UI Tracking/Tracker Behaviours/AssignMainCameraOnTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Hairibar.UI.Tracking
+{
+    public class AssignMainCameraOnTracker : BaseUITrackerBehaviour
+    {
+        private void Start()
+        {
+            Camera mainCamera = Camera.main;
+            Assert.IsNotNull(mainCamera, "There is no Main Camera to auto assign.");
+            Tracker.camera = mainCamera;
+
+            Destroy(this);
+        }
+    }
+
+}
