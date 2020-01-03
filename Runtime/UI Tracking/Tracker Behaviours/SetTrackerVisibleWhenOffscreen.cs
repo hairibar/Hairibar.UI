786535584984e6ad9d6a61bebf6ff36c8278f460
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-03 14:41:42 +0100

    Added SetTrackerVisibleWhenOffscreen

---
+++ Runtime/UI Tracking/Tracker Behaviours/SetTrackerVisibleWhenOffscreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+#pragma warning disable 649
+namespace Hairibar.UI.Tracking
+{
+    public class SetTrackerVisibleWhenOffscreen : BaseUITrackerBehaviour
+    {
+        #region Inspector
+        /// <summary>
+        /// If true, the tracker will be visible when on screen and invisible when off screen.
+        /// </summary>
+        [Tooltip("If true, the tracker will be visible when on screen and invisible when off screen.")]
+        public bool invertVisible = false;
+        #endregion
+
+        private bool TrackedObjectIsOnScreen
+        {
+            get
+            {
+                Vector3 positionInMainViewport = Tracker.camera.WorldToViewportPoint(Tracker.TrackedObject.transform.position);
+
+                return (positionInMainViewport.x > 0 && positionInMainViewport.x < 1 &&
+                    positionInMainViewport.y > 0 && positionInMainViewport.y < 1);
+            }
+        }
+
+        private void Update()
+        {
+            Tracker.IsVisible = TrackedObjectIsOnScreen == invertVisible;
+        }
+    }
+}
+
+
