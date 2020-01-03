7a6086ed577d904ddc2d39f869a17a6136123786
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-03 14:10:30 +0100

    Some folder and assembly definition reorganization

---
+++ Runtime/Menu Screen Management/BackButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Hairibar.UI.MenuScreenManagement
+{
+    /// <summary>
+    /// Button that calls GoBack on a parent MenuScreenManager.
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class BackButton : MonoBehaviour
+    {
+        /// <summary>
+        /// Goes back from the current MenuScreen.
+        /// </summary>
+        public void GoBack()
+        {
+            AMenuScreen menuScreen = GetComponentInParent<AMenuScreen>();
+            if (menuScreen) menuScreen.GoBack();
+            else Debug.LogError("BackButton isn't a child of any AMenuScreen.", this);
+        }
+
+        private void Start()
+        {
+            UnityEvent onClick = GetComponent<Button>().onClick;
+
+            for (int i = 0; i < onClick.GetPersistentEventCount(); i++)
+            {
+                if (onClick.GetPersistentTarget(i) == this)
+                {
+                    onClick.SetPersistentListenerState(i, UnityEventCallState.Off);
+                }
+            }
+
+            onClick.AddListener(GoBack);
+        }
+    }
+}
+
---
+++ Runtime/Small Functionalities/InvokeOnClickWhenKeyPressed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hairibar.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class InvokeOnClickWhenKeyPressed : MonoBehaviour
+    {
+        public KeyCode key = KeyCode.Return;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(key))
+            {
+                GetComponent<Button>().onClick.Invoke();
+            }
+        }
+    }
+
+}
