e5e5865c4696ff561f79437bfa72888594db2fca
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-04 11:42:20 +0100

    Added MatchTrackedObjectScale

---
+++ Runtime/UI Tracking/Tracker Behaviours/MatchTrackedObjectScale.cs
@@ -0,0 +1,13 @@
+﻿namespace Hairibar.UI.Tracking
+{
+    /// <summary>
+    /// Matches the local scale of the tracked object.
+    /// </summary>
+    public class MatchTrackedObjectScale : BaseUITrackerBehaviour
+    {
+        private void Update()
+        {
+            transform.localScale = Tracker.TrackedObject.transform.localScale;
+        }
+    }
+}
