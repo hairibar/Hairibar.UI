f31d1272ab6abb5b592dc641ad30d714f7589c3a
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-04 12:00:51 +0100

    Made screen clamping optional in ScreenSpacePositionTracker. Closes #6.

--- Runtime/UI Tracking/Tracker Behaviours/ScreenSpacePositionTracker.cs
+++ Runtime/UI Tracking/Tracker Behaviours/ScreenSpacePositionTracker.cs
@@ -1,36 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Hairibar.UI.Tracking
 {
+    /// <summary>
+    /// Matches the tracked object's screen space position.
+    /// </summary>
     public class ScreenSpacePositionTracker : BaseUITrackerBehaviour
     {
+        /// <summary>
+        /// Should the tracker stay at the screen's border when the tracked object goes off the screen?
+        /// </summary>
+        [Tooltip("Should the tracker stay at the screen's border when the tracked object goes off the screen?")]
+        public bool clampInsideScreen = true;
+
+
         private RectTransform rectTransform;
+
 
         private void Update()
         {
-            Vector3 newPos = transform.position;
+            Vector3 targetPosition = Tracker.TargetPositionInScreenSpace;
 
-            Vector3 unclampedPos = Tracker.camera.WorldToViewportPoint(Tracker.TrackedObject.transform.position);
-
-            Vector3 normalizedPos;
-            normalizedPos.x = Mathf.Clamp01(unclampedPos.x);
-            normalizedPos.y = Mathf.Clamp01(unclampedPos.y);
-            normalizedPos.z = unclampedPos.z;
-
-            rectTransform.anchorMin = normalizedPos;
-            rectTransform.anchorMax = normalizedPos;
+            if (clampInsideScreen)
+            {
+                targetPosition.x = Mathf.Clamp01(targetPosition.x);
+                targetPosition.y = Mathf.Clamp01(targetPosition.y);
+            }
+            
+            rectTransform.anchorMin = targetPosition;
+            rectTransform.anchorMax = targetPosition;
 
             Rect viewRect = rectTransform.rect;
             Vector2 anchoredPosition = Vector2.zero;
 
-            if (normalizedPos.x == 0) anchoredPosition.x = viewRect.width / 2;
-            else if (normalizedPos.x == 1) anchoredPosition.x = -viewRect.width / 2;
+            if (clampInsideScreen)
+            {
+                if (targetPosition.x == 0) anchoredPosition.x = viewRect.width / 2;
+                else if (targetPosition.x == 1) anchoredPosition.x = -viewRect.width / 2;
 
-            if (normalizedPos.y == 0) anchoredPosition.y = viewRect.height / 2;
-            else if (normalizedPos.y == 1) anchoredPosition.y = -viewRect.height / 2;
-
+                if (targetPosition.y == 0) anchoredPosition.y = viewRect.height / 2;
+                else if (targetPosition.y == 1) anchoredPosition.y = -viewRect.height / 2;
+            }
+            
             rectTransform.anchoredPosition = anchoredPosition;
         }
 
