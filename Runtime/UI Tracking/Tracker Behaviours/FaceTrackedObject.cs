41818409863beb4e527ec8fab59dca596f7e0dcf
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-04 12:25:30 +0100

    Added FaceTrackedObject. Closes #1.

---
+++ Runtime/UI Tracking/Tracker Behaviours/FaceTrackedObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Hairibar.UI.Tracking
+{
+    /// <summary>
+    /// Rotates the tracker to face the tracked object.
+    /// </summary>
+    public class FaceTrackedObject : BaseUITrackerBehaviour
+    {
+        [SerializeField, Tooltip("The direction that the tracker faces towards without applying any rotation.")]
+        private Vector2 baseFacing;
+
+        private void Update()
+        { 
+            float angle = Vector2.SignedAngle(baseFacing, Tracker.TargetPositionInScreenSpace - Tracker.TrackerPositionInScreenSpace);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }   
+    }
+}
+
