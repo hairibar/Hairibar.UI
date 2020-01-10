2f6e33e499a4d58942c85c0d9445c2c296e1c3b0
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-10 11:08:26 +0100

    Added a custom editor to AMenuScreen that shows an error message if it doesn't have a MenuScreenManager as a parent.

---
+++ Editor/AMenuScreenEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace Hairibar.UI.MenuScreenManagement
+{
+    [CustomEditor(typeof(AMenuScreen), true)]
+    class AMenuScreenEditor : Editor
+    {
+        private bool HasManagerAsParent => (target as AMenuScreen).GetComponentInParent<MenuScreenManager>();
+
+        public override void OnInspectorGUI()
+        {
+            if (!HasManagerAsParent) EditorGUILayout.HelpBox("AMenuScreens must be children of a MenuScreenManager to be functional.", MessageType.Error);
+            else DrawDefaultInspector();
+        }
+    }
+}
